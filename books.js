a96eabcac57523399a5940075c28ca5e1507b29b
glofaydigitalbookstore <glofaydigitalbookstore@gmail.com>
2026-01-07 15:17:12 +0100

    Add files via upload

---
+++ books.js
@@ -0,0 +1,42 @@
+const books = [
+  {
+    id: 1,
+    title: "Satan Get Lost",
+    author: "David Oyedepo",
+    price: 600,
+    file: "ebooks/Satan_Get_Lost.pdf",  
+    image: "ebooks/Satan_Get_Lost_cover.webp"
+  },
+  {
+    id: 2,
+    title: "A Believer's Guide To Effective Fasting & Prayer",
+    author: "Faith A. Oyedepo",
+    price: 1600,
+    file: "ebooks/A_Believers_Guide_To_Effective_Fasting_And_Prayer.pdf",
+    image: "ebooks/A_Believers_Guide_To_Effective_Fasting_And_Prayer_cover.jpeg"
+  },
+  {
+    id: 3,
+    title: "Success Buttons",
+    author: "David Oyedepo",
+    price: 800,
+    file: "ebooks/Success_Buttons.pdf",
+    image: "ebooks/Success_Buttons_cover.webp"
+  },
+  {
+    id: 4,
+    title: "Born To Win",
+    author: "David Oyedepo",
+    price: 900,
+    file: "ebooks/Born_To_Win.pdf",
+    image: "ebooks/Born_To_Win_cover.webp"
+  },
+  {
+    id: 5,
+    title: "Winning Prayer",
+    author: "David Oyedepo",
+    price: 1200,
+    file: "ebooks/Winning_Prayer.pdf",
+    image: "ebooks/Winning_Prayer_cover.webp"
+  }
+];

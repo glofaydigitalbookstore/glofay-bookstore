d2deccaecb3ce27cbbf1e3e056fdf349898f767e
glofaydigitalbookstore <glofaydigitalbookstore@gmail.com>
2026-01-08 05:52:42 +0100

    Update books.js

--- books.js
+++ books.js
@@ -1,14 +1,8 @@
 // ====== BOOK DATA ======
   const books = [
     { id:1, title:"Satan Get Lost", author:"David Oyedepo", price:600, image:"satan_get_lost.jpeg" },
-    { id:2, title:"Fasting and Prayer", author:"Faith A. Oyedepo", price:1000, image:"a_believers_guide_to_effective_fasting_prayer.jpeg" },
+    { id:2, title:"Fasting and Prayer", author:"Faith A. Oyedepo", price:1000, image:"a_believers_guide_to_effective_fasting_and_prayer.jpeg" },
     { id:3, title:"Success Buttons", author:"David Oyedepo", price:800, image:"success_buttons.jpeg" },
     { id:4, title:"Born To Win", author:"David Oyedepo", price:900, image:"born_to_win.jpeg" },
     { id:5, title:"Winning Prayer", author:"David Oyedepo", price:1200, image:"winning_prayer.jpeg" }
   ];
-
-  const bookList = document.getElementById("bookList");
-  const cartItems = document.getElementById("cartItems");
-  const total = document.getElementById("total");
-
-  let cart = [];

cdc123f3ba44348061b8ab43facfb8709dd3661c
glofaydigitalbookstore <glofaydigitalbookstore@gmail.com>
2026-01-12 02:44:56 +0100

    Update books.js

--- books.js
+++ books.js
@@ -1,14 +1,15 @@
 // ====== BOOK DATA ======
-  const books = [
-    { id:1, title:"Satan Get Lost", author:"David Oyedepo", price:600, image:"satan_get_lost.jpeg" },
-    { id:2, title:"Fasting and Prayer", author:"Faith A. Oyedepo", price:1000, image:"a_believers_guide_to_effective_fasting_and_prayer_cover.jpeg" },
-    { id:3, title:"Success Buttons", author:"David Oyedepo", price:800, image:"success_buttons.jpeg" },
-    { id:4, title:"Born To Win", author:"David Oyedepo", price:900, image:"born_to_win.jpeg" },
-    { id:5, title:"Winning Prayer", author:"David Oyedepo", price:1200, image:"winning_prayer.jpeg" }
-  ];
+ const books = [
+  { id:1, title:"Satan Get Lost", author:"David Oyedepo", price:600, image:"satan_get_lost.jpeg" },
+  { id:2, title:"A Believers Guide To Effective Fasting and Prayer", author:"Faith A. Oyedepo", price:1000, image:"a_believers_guide_to_effective_fasting_and_prayer_cover.jpeg" },
+  { id:3, title:"Success Buttons", author:"David Oyedepo", price:800, image:"success_buttons.jpeg" },
+  { id:4, title:"Born To Win", author:"David Oyedepo", price:900, image:"born_to_win.jpeg" },
+  { id:5, title:"Winning Prayer", author:"David Oyedepo", price:1200, image:"winning_prayer.jpeg" },
 
-const bookList = document.getElementById("bookList");
-  const cartItems = document.getElementById("cartItems");
-  const total = document.getElementById("total");
-
-  let cart = [];
+  { id:6, title:"Prayer Rain", author:"D. K. Olukoya", price:1000, image:"prayer_rain.jpg" },
+  { id:7, title:"Supernatural Childbirth", author:"Jackie Mize", price:1000, image:"supernatural_childbirth.jpg" },
+  { id:8, title:"Turnaround Power of the Word", author:"David Oyedepo", price:900, image:"turnaround_power_of_the_word.jpeg" },
+  { id:9, title:"Understanding the Power of Praise", author:"David Oyedepo", price:900, image:"understanding_the_power_of_praise.webp" },
+  { id:10, title:"Memory Verse Flash Cards", author:"Faithful Ayana", price:3000, image:"memory_verse_flash_cards.png" },
+  { id:11, title:"Walking in the Newness of Life", author:"David Oyedepo", price:900, image:"walking_in_the_newness_of_life.jpg" }
+];
